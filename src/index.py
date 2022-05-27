9a2390a5797d98e32cfa52de6ed607fd2a3f412b
benxh7 <banjaminmorales571@gmail.com>
2022-05-27 03:03:53 -0400

    Primera version del Bot hecho en Python

---
+++ src/index.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from AntiScam import AntiScam
+
+whitelist = [500095712348995584, 799431815249133659] # Here you can add the IDs of the users allowed to bypass the AntiScam system.
+logs_channel = None # Here you can add the ID of the channel where the logs will be sent.
+
+bot = commands.Bot(command_prefix='>')
+bot.remove_command('help') # Remove this line if you want to use the help command.
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="Moderar el Discord"))
+    print("Iniciando... Keytan Link Blocker, estamos listos para bloquear.")
+
+@bot.listen()
+async def on_message(message):
+    await AntiScam(message, bot = bot, whitelist = whitelist, muted_role='Muted', verified_role='Miembro', logs_channel=925238825062629397) # Here you can change the names of the roles.
+
+bot.run('OTc5NjE5NjY3MTYzMzYxMzIw.GHxWg2.3GGyg26LdOZXUOcGAI1wrXNz8uwMy3pgOgEd7A')
